538049f2a9f990451b80c667b0ea688147de0945
Chxrgxr <137828241+Chxrgxr@users.noreply.github.com>
2024-01-22 23:58:18 -0600

    4

---
+++ esign.php
@@ -0,0 +1,9 @@
+<?php
+<!DOCTYPE html>
+<html>
+<body>
+
+<meta http-equiv="Refresh" content="3; url="plists/esign.plist"
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>IPASigner</title>
+</head>
+<body>
+
+<form action="esign.php" method="post">
+    <button>Sign ESign</button>
+</form>
+
+</body>
+</html>
